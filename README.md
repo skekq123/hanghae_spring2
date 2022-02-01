8f35d79c29c8ef7835dee0be04846cc23035a03b
고영빈 <skekq123@naver.com>
2022-02-01 20:12:36 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hanghae_spring2
+
 스프링 주특기 공부2
---
+++ src/main/java/com/example/hanghae_spring2/HanghaeSpring2Application.java
@@ -0,0 +1,13 @@
+package com.example.hanghae_spring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeSpring2Application.class, args);
+    }
+
+}
