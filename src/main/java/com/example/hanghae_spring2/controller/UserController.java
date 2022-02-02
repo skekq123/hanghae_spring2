1fc67db6ea1573a873b77a6ceb5a354156d650a8
고영빈 <skekq123@naver.com>
2022-02-02 19:13:02 +0900

    카카오 로그인기능 추가

--- src/main/java/com/example/hanghae_spring2/controller/UserController.java
+++ src/main/java/com/example/hanghae_spring2/controller/UserController.java
@@ -1,19 +1,25 @@
 package com.example.hanghae_spring2.controller;
 
+import com.example.hanghae_spring2.dto.KakaoUserInfoDto;
 import com.example.hanghae_spring2.dto.SignupRequestDto;
+import com.example.hanghae_spring2.service.KakaoUserService;
 import com.example.hanghae_spring2.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
     private final UserService userService;
+    private final KakaoUserService kakaoUserService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, KakaoUserService kakaoUserService) {
         this.userService = userService;
+        this.kakaoUserService = kakaoUserService;
     }
 
     //로그인
@@ -32,5 +38,11 @@ public class UserController {
     public String registerUser(SignupRequestDto requestDto) {
         userService.registerUser(requestDto);
         return "redirect:/user/login";
+    }
+    //카카오 로그인
+    @GetMapping("/user/kakao/callback")
+    public String kakaoLogin(@RequestParam String code) throws JsonProcessingException {
+        kakaoUserService.kakaoLogin(code);
+        return "redirect:/";
     }
 }
